a3be0580e5a8914fd0e010a96b1e73a107362332
kumarisunita16 <145357980+kumarisunita16@users.noreply.github.com>
2023-10-12 17:49:40 +0530

    12 oct

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,56 @@
+import { Component, NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { HomeComponent } from './home/home.component';
+import { ServiceComponent } from './service/service.component';
+import { ContactComponent } from './contact/contact.component';
+import { PackageComponent } from './package/package.component';
+import { ResisterComponent } from './resister/resister.component';
+
+const routes: Routes = [
+  {
+    component:HomeComponent,
+    path:''
+  },
+  {
+    component:HomeComponent,
+    path:'home'
+  },
+  {
+    component:AboutComponent,
+    path:'about',
+    children:[{
+      component:ServiceComponent,
+      path:'service'
+    }
+     ]
+  },
+  {
+    component:ServiceComponent,
+    path:'service'
+  },
+  {
+    component:PackageComponent,
+    path:'package',
+    children:[
+      {
+        component:ResisterComponent,
+        path:'resister'
+      }
+    ]
+  },
+  {
+    component:ContactComponent,
+    path:'contact'
+  },
+  {
+    component:ResisterComponent,
+    path:'resister'
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ src/app/app.module.ts
@@ -0,0 +1,35 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ServiceComponent } from './service/service.component';
+import { ContactComponent } from './contact/contact.component';
+import { PackageComponent } from './package/package.component';
+import { ResisterComponent } from './resister/resister.component';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+// implements{FormsModule}
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    AboutComponent,
+    ServiceComponent,
+    ContactComponent,
+    PackageComponent,
+    ResisterComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
